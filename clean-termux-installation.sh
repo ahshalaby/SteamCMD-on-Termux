b35d94aecdd1facfe56f65b5c39f075fa1ecfbbb
ahshalaby <150528958+ahshalaby@users.noreply.github.com>
2023-12-19 21:34:50 +0200

    Create clean-termux-installation.sh

---
+++ clean-termux-installation.sh
@@ -0,0 +1,31 @@
+echo -e "Please press Allow to grant Termux access to the internal storage."
+termux-setup-storage
+
+echo -e "This script will install proot-distro, install Debian on it and then run the script that will install box86-64 and SteamCMD."
+read -n 1 -s -r -p "Press any key to continue."
+echo -e
+apt update && apt upgrade -y
+
+echo -e "Installing proot-distro..."
+apt install proot-distro -y
+
+echo -e "Installing Debian..."
+proot-distro install debian
+
+echo -e "Downloading and starting the install script..."
+wget https://github.com/ahshalaby/SteamCMD-on-Termux/blob/main/install_box86-64_steamcmd.sh
+mv install_box86-64_steamcmd.sh $PREFIX/var/lib/proot-distro/installed-rootfs/debian/root
+proot-distro login debian --user root --shared-tmp -- bash -c ./install_box86-64_steamcmd.sh
+
+rm $PREFIX/var/lib/proot-distro/installed-rootfs/debian/root/install_box86-64_steamcmd.sh
+
+echo '#!/data/data/com.termux/files/usr/bin/bash
+
+proot-distro login debian --user root --shared-tmp -- bash -c steamcmd' >> steamcmd
+chmod +x steamcmd
+mv steamcmd /data/data/com.termux/files/usr/bin
+
+echo -e "Installation completed. Now you can start SteamCMD either by typing steamcmd in the Termux or Debian console."
+echo -e "NOTE: SteamCMD was configured to install to the Steam folder in the internal storage (will appear with first launch)."
+read -n 1 -s -r -p "Press any key to continue."
+echo -e
