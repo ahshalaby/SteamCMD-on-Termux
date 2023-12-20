a75c0843cb05474fe0d6bfe102af7c58cddb554b
ahshalaby <150528958+ahshalaby@users.noreply.github.com>
2023-12-20 06:53:50 +0200

    Update install_in_clean_termux.sh

--- install_in_clean_termux.sh
+++ install_in_clean_termux.sh
@@ -33,7 +33,7 @@ wget https://github.com/ahshalaby/SteamCMD-on-Termux/blob/main/option_scripts/st
 mv steamcmd-options /data/data/com.termux/files/usr/bin
 cd
 
-echo -e "\nInstallation completed.\nNow you can start SteamCMD either by typing steamcmd in the Termux or Debian console.\nYou can tweak SteamCMD's launch options by typing steamcmd-options in Termux.\n"
+echo -e "\nInstallation completed.\nNow you can start SteamCMD either by typing \033[0;32msteamcmd \033[0;37min the Termux or Debian console.\nYou can tweak SteamCMD's launch options by typing \033[0;32msteamcmd-options \033[0;37min Termux.\n"
 echo -e "NOTE: SteamCMD was configured to install to the Steam folder in the internal storage (will appear with first launch)."
 read -n 1 -s -r -p "Press any key to continue."
 echo -e "\n"
