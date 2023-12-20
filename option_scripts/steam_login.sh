a938583d448f42c77bedcd7e8dc9cfb17f16018e
ahshalaby <150528958+ahshalaby@users.noreply.github.com>
2023-12-20 02:06:18 +0200

    Create steam_login.sh
    Stores username and password (optional) to be later called in the bin file.

---
+++ option_scripts/steam_login.sh
@@ -0,0 +1,55 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+while true; do
+
+while true; do
+read -p "Username: " un
+
+case "$un" in
+*\ * ) echo -e "Username can not contain spaces. Please try again or type \033[0;32mcancel \033[0;37mto exit.";;
+*) break;;
+esac
+
+done
+
+if [[ "$un" = "cancel" ]]; then
+exit
+fi
+
+[ -n "$un" ] && break || echo -e "\nUsername can't be empty. Please try again or type \033[0;32mcancel \033[0;37mto exit."
+echo -e "\n"
+done
+
+while true; do
+
+while true; do
+read -s -p "Password (can be left empty): " pw
+echo
+
+if [[ "$pw" = "cancel" ]]; then
+exit
+fi
+
+case "$pw" in 
+*\ *) echo -e "Password can not contain spaces. Please try again or type \033[0;32mcancel \033[0;37mto exit.";;
+*) break ;;
+esac
+
+done
+
+if [[ -z "$pw" ]]; then cd $PREFIX/var/lib/proot-distro/installed-rootfs/debian/root/.steamcmd && rm usrdata && echo "$un" >> usrdata && cd
+exit
+fi
+
+read -s -p "Password (again): " pw2
+echo 
+
+[ "$pw" = "$pw2" ] && break || echo -e "\nPasswords do not match. Please try again."
+read -n 1 -s -r -p "Press any key to retry."
+echo -e "\n"
+done
+
+cd $PREFIX/var/lib/proot-distro/installed-rootfs/debian/root/.steamcmd
+rm usrdata
+echo "$un $pw" >> usrdata
+cd
