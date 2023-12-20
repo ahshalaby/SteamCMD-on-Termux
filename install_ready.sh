fb5efbf1a572a2a4da3b3c8825e915a7900979db
ahshalaby <150528958+ahshalaby@users.noreply.github.com>
2023-12-20 09:44:38 +0200

    Update install_ready.sh

--- install_ready.sh
+++ install_ready.sh
@@ -6,7 +6,8 @@ read -n 1 -s -r -p "Press any key to continue."
 echo -e "\n"
 
 echo -e "\nDownloading and starting the install script...\n"
-wget https://github.com/ahshalaby/SteamCMD-on-Termux/blob/main/install_box86-64_steamcmd.sh
+wget https://raw.githubusercontent.com/ahshalaby/SteamCMD-on-Termux/main/install_box86-64_steamcmd.sh
+
 mv install_box86-64_steamcmd.sh $PREFIX/var/lib/proot-distro/installed-rootfs/debian/root
 proot-distro login debian --user root --shared-tmp -- bash -c ./install_box86-64_steamcmd.sh
 
@@ -20,10 +21,10 @@ mv steamcmd /data/data/com.termux/files/usr/bin
 
 echo -e "\nDownloading SteamCMD options scripts...\n"
 mkdir option_scripts && cd option_scripts
-wget https://github.com/ahshalaby/SteamCMD-on-Termux/blob/main/option_scripts/steam_installdir.sh && chmod +x steam_installdir.sh
-wget https://github.com/ahshalaby/SteamCMD-on-Termux/blob/main/option_scripts/steam_login.sh && chmod +x steam_login.sh
-wget https://github.com/ahshalaby/SteamCMD-on-Termux/blob/main/option_scripts/steam_platform.sh && chmod +x steam_platform.sh
-wget https://github.com/ahshalaby/SteamCMD-on-Termux/blob/main/option_scripts/steamcmd-options && chmod +x steamcmd-options
+wget https://raw.githubusercontent.com/ahshalaby/SteamCMD-on-Termux/main/option_scripts/steam_installdir.sh && chmod +x steam_installdir.sh
+wget https://raw.githubusercontent.com/ahshalaby/SteamCMD-on-Termux/main/option_scripts/steam_login.sh && chmod +x steam_login.sh
+wget https://raw.githubusercontent.com/ahshalaby/SteamCMD-on-Termux/main/option_scripts/steam_platform.sh && chmod +x steam_platform.sh
+wget https://raw.githubusercontent.com/ahshalaby/SteamCMD-on-Termux/main/option_scripts/steamcmd-options && chmod +x steamcmd-options
 mv steamcmd-options /data/data/com.termux/files/usr/bin
 cd
 
@@ -34,4 +35,3 @@ echo -e "\nInstallation completed.\nNow you can start SteamCMD either by typing 
 echo -e "NOTE: SteamCMD was configured to install to the Steam folder in the internal storage (will appear with first launch)."
 read -n 1 -s -r -p "Press any key to continue."
 echo -e "\n"
-
