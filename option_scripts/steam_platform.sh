59677f61d1bd83154684954b410243a6508e22eb
ahshalaby <150528958+ahshalaby@users.noreply.github.com>
2023-12-26 13:14:08 +0200

    Update steam_platform.sh

--- option_scripts/steam_platform.sh
+++ option_scripts/steam_platform.sh
@@ -1,21 +1,21 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-echo -e "\nSelect your desired platform:\n\n| Leave empty for default (\033[0;32mWindows\033[0;37m) |\n"
-echo -e "1- \033[0;32mWindows\033[0;37m"
-echo -e "2- \033[0;32mLinux\033[0;37m"
+echo -e "\nSelect your desired platform:\n\n| Leave empty for default (\033[0;32mLinux\033[0;37m) |\n"
+echo -e "1- \033[0;32mLinux\033[0;37m"
+echo -e "2- \033[0;32mWindows\033[0;37m"
 echo -e "3- \033[0;32mMacOS\033[0;37m\n"
 
 read -p "Select an option:" option
 case $option in
+
 1) cd $PREFIX/var/lib/proot-distro/installed-rootfs/debian/root/.steamcmd_config
 rm pltfrm 2> /dev/null
-echo "+@sSteamCmdForcePlatformType windows" >> pltfrm
 cd
 exit;;
 
 2) cd $PREFIX/var/lib/proot-distro/installed-rootfs/debian/root/.steamcmd_config
 rm pltfrm 2> /dev/null
-echo "+@sSteamCmdForcePlatformType linux" >> pltfrm
+echo "+@sSteamCmdForcePlatformType windows" >> pltfrm
 cd
 exit;;
 
@@ -27,7 +27,6 @@ exit;;
 
 *) cd $PREFIX/var/lib/proot-distro/installed-rootfs/debian/root/.steamcmd_config
 rm pltfrm 2> /dev/null
-echo "+@sSteamCmdForcePlatformType windows" >> pltfrm
 cd
 exit;;
 esac
